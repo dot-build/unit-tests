020e3a94ea532c3d06d409cf5b9f55e6705bb865
Darlan Alves <me@darlanalv.es>
2022-10-04 01:25:57 +0200

    fix: update jest dependencies

--- jest/index.mjs
+++ jest/index.mjs
@@ -1,14 +1,14 @@
 import { copyFiles, installDependencies, updateScripts } from '../common.mjs';
 
 const dependencies = {
-  '@types/jest': '^27.0.1',
-  '@types/node': '^14.14.31',
-  jest: '^27.1.0',
-  'ts-jest': '^27.0.5',
-  'ts-jest-resolver': '^1.1.0',
-  'ts-node': '^9.1.1',
-  typescript: '^4.1.3',
-  prettier: '^2.3.2',
+  "@types/jest": "^29.1.1",
+  "@types/node": "^18.8.1",
+  "jest": "^29.1.2",
+  "prettier": "^2.7.1",
+  "ts-jest": "^29.0.3",
+  "ts-jest-resolver": "^2.0.0",
+  "ts-node": "^10.9.1",
+  "typescript": "^4.8.4",
 };
 
 const files = ['jest.config.ts', 'tsconfig.json', '.prettierrc'];
